a0c9744167a862c7add0ab860e55210e905dbefa
Piyush Nigam <dev.piyushnigam@gmail.com>
2017-08-08 23:44:33 -0700

    Updating index.js to work with hook.io

--- webhook/javascript/index.js
+++ webhook/javascript/index.js
@@ -4,41 +4,38 @@
 //* @param {Object} req Cloud Function request context.
 //* @param {Object} res Cloud Function response context.
 //*/
-//exports.helloHttp = function helloHttp(req, res) {
-//	response = "This is a sample response from your webhook!" // Default repsonse from the Webhook to show its working.
-//	res.setHeader('Content-Type', 'application/json');
-//	res.send(JSON.stringify({"speech": response, "displayText": response}));
-//};
 
-
-// --- New code start from here --- //
 
 'use strict';
 const http = require('http');
 const host = 'http://api.worldweatheronline.com/premium/v1/weather.ashx';
 const wwoApiKey = 'c08e48798bfc4cfe97b62316170508';
 
-exports.weatherWebhook = (req,res) => {
-let city = req.body.result.parameters['geo-city'];
+module['exports'] = function handleRequest(hook) {
 
-let date = ''
-  if (req.body.result.parameters['date']) {
-    date = req.body.result.parameters['date'];
+  let city = hook.params['geo-city'];
+
+  console.log('Hook: ');
+  console.log(hook);
+  // Return weather for today by default.
+  let date = 'today';
+  if (hook.params['date']) {
+    date = hook.params['date'];
     console.log('Date: ' + date);
   }
   // Call the weather API.
   callWeatherApi(city, date).then((output) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({"speech": output, "displayText": output}));
+    hook.res.setHeader('Content-Type', 'application/json');
+    hook.res.end(JSON.stringify({"speech": output, "displayText": output}));
   }).catch((error) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({"speech": error, "displayText": error}));
+    hook.res.setHeader('Content-Type', 'application/json');
+    hook.res.end(JSON.stringify({"speech": error, "displayText": error}));
   });
 };
 
 function callWeatherApi(city, date) {
   return new Promise((resolve, reject) => {
-    let path = '/premium/v1/weather.ashx?format=json&num_of_days=1' +
+    let path = '?format=json&num_of_days=1' +
       '&q=' + encodeURIComponent(city) + '&date=' + date + '&key=' + wwoApiKey;
     console.log('API Request: ' + host + path);
  
